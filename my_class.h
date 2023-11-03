7fc46480b0de17d7310e9b6a0aa45892b7ca92b0
nathaly johansson <na3162jo-s@student.lu.se>
2023-11-03 18:33:52 +0100

    my header file

--- my_class.h
+++ my_class.h
@@ -5,19 +5,17 @@
 
 class my_class : public TObject {
 public:
-    my_class(); // default constructor
-    my_class(Double_t px, Double_t py, Double_t pz); // constructor with initialization
-    virtual ~my_class(); // destructor
+    my_class(); 
+    my_class(Double_t px, Double_t py, Double_t pz); 
+    virtual ~my_class(); 
 
-    // Add members for px, py, pz
     Double_t px;
     Double_t py;
     Double_t pz;
 
-    // Member function to calculate the magnitude
     Double_t GetMagnitude() const;
 
-    ClassDef(my_class, 1); // my_class
+    ClassDef(my_class, 1);
 };
 
-#endif // __MY_CLASS_H__
+#endif 
